1ddd1ee567ea40e446fd8ec2aca388de51f464db
DallinRodgers <rodgers.dallin@gmail.com>
2020-01-26 22:27:43 -0700

    Update seeds.sql
    created insert statements

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sweet Chili O'Mine Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bohemian Radishy Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("It’s Fun To Eat At The RYeMCA Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("I Fought The Slaw Burger (And The Slaw Won)", false);
