5fadd0c90fcb7a829da9120f7a9bd2834e07e515
DallinRodgers <rodgers.dallin@gmail.com>
2020-01-29 19:28:11 -0700

    Update orm.js
    Created insertOne and updateOne

--- config/orm.js
+++ config/orm.js
@@ -7,6 +7,20 @@ var orm = {
       if (err) throw err;
       console.log(result);
     });
+  },
+  insertOne: function(burgerName, isDevoured){
+    var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, ?);";
+    connection.query(queryString, [burgerName, isDevoured], function(err, result){
+      if(err) throw err;
+      console.log(result);
+    })
+  },
+  updateOne: function(updateThisOne, isDevoured){
+    var queryString = "UPDATE burgers SET devoured = ? WHERE id = ?";
+    connection.query(queryString, [isDevoured, updateThisOne], function(err, result){
+      if(err) throw err;
+      console.log("Burger is Devoured");
+    })
   }
 };
 
