ee73854cfdd58ce638289568c5e6e98887d8e189
DallinRodgers <rodgers.dallin@gmail.com>
2020-01-29 20:06:22 -0700

    Update orm.js
    addec callback

--- config/orm.js
+++ config/orm.js
@@ -2,24 +2,27 @@ var connection = require("./connection.js");
 
 // selectall, insertone, updateone
 var orm = {
-  selectAll: function() {
+  selectAll: function(callBack) {
     connection.query("SELECT * FROM burgers", function(err, result) {
       if (err) throw err;
       console.log(result);
+      callBack(result);
     });
   },
-  insertOne: function(burgerName, isDevoured){
+  insertOne: function(burgerName, isDevoured, callBack){
     var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, ?);";
     connection.query(queryString, [burgerName, isDevoured], function(err, result){
       if(err) throw err;
       console.log(result);
+      callBack(result);
     })
   },
-  updateOne: function(updateThisOne, isDevoured){
+  updateOne: function(updateThisOne, isDevoured, callBack){
     var queryString = "UPDATE burgers SET devoured = ? WHERE id = ?";
     connection.query(queryString, [isDevoured, updateThisOne], function(err, result){
       if(err) throw err;
       console.log("Burger is Devoured");
+      callBack(result);
     })
   }
 };
