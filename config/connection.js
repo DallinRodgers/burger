edc00b89466c347b222f6c8c239e401683c27209
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-01-29 18:07:17 -0700

    Update connection.js
    Started connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
-// Password from .env file
-const password = process.env.DB_PASS;
+var mysql = require("mysql");
 
 // Connect to MYSQL Database
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: password,
-  database: "bamazon_db"
+  password: "",
+  database: "burgers_db"
 });
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
