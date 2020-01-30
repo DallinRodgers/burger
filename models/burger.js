e74bef654ff345a8ea49d91954ac2087737d1bd6
DallinRodgers <rodgers.dallin@gmail.com>
2020-01-29 20:06:33 -0700

    Update burger.js
    created burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,27 @@
-var orm = require('../config/orm')
+var orm = require('../config/orm.js')
 
 // Works
 // orm.insertOne("This is a test", false);
 // Works
 // orm.updateOne(2, true);
 orm.selectAll();
+
+var burger = {
+    all: function(cb){
+        orm.selectAll(function(result){
+            cb(result);
+        })
+    },
+    create: function(burgerName, isDevoured, cb){
+        orm.insertOne(burgerName, isDevoured, function(result){
+            cb(result);
+        });
+    },
+    update: function(updateThisOne, isDevoured, cb){
+        orm.updateOne(updateThisOne, isDevoured, function(result){
+            cb(result);
+        })
+    }
+}
+
+module.exports = burger;
