11fef82a97cd377e7414e2b5619f700ffdae5fe3
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-01-29 18:07:06 -0700

    Update server.js
    Added some express settings,
    connected file to orm.js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
 var PORT = process.env.PORT || 8080;
 
+// Password from .env file
+const password = process.env.DB_PASS;
+console.log("server.js " + password);
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var orm = require("./config/orm");
+
+orm.selectAll();
+
 app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
